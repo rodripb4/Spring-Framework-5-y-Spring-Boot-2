92e8efbf0974c5220fd1d4185fae5a6b66b7a602
Rodri <rodripb4@hotmail.com>
2022-10-02 20:02:27 +0200

    Upload de archivos

--- srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SrpingBootDataJpaApplication.java
+++ srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SrpingBootDataJpaApplication.java
@@ -1,13 +1,28 @@
 package com.bolsaideas.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bolsaideas.springboot.app.models.service.IUploadFileService;
+
 @SpringBootApplication
-public class SrpingBootDataJpaApplication {
+public class SrpingBootDataJpaApplication implements CommandLineRunner{
+	
+	@Autowired
+	IUploadFileService uploadFileService;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SrpingBootDataJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+		
 	}
 
 }
--- srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
+++ srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
@@ -43,6 +43,7 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
+	private String foto;
 
 	public Long getId() {
 		return id;
@@ -87,5 +88,15 @@ public class Cliente implements Serializable {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
 
 }
