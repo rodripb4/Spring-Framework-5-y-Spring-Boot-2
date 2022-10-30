b75733ceaed671fd128e69b9ddc5988ab45630a6
Rodri <rodripb4@hotmail.com>
2022-10-30 23:01:03 +0100

    Cambiados todos los textos estaticos para que se cambien en funcion del idioma seleccionado

--- srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SpringSecurityConfig.java
+++ srping-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SpringSecurityConfig.java
@@ -48,7 +48,7 @@ public class SpringSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**").permitAll()
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar", "/locale").permitAll()
 				.anyRequest()
 				.authenticated()
 				.and()
